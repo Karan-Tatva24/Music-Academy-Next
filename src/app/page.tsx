ac1b4fd96366edd971881219b552e4ad2c05c7cb
Karan-Tatva24 <karan.sarvaiya@etatvasoft.com>
2024-07-23 18:14:51 +0530

    Add hero section of dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <main className="bg-grid-white/[0.2] min-h-screen bg-black/[0.96] antialiased">
-      <h1 className="text-center text-3xl">Hi Karan</h1>
+      <HeroSection />
     </main>
   );
 }
