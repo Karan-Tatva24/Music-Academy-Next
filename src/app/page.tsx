de973f71af14dadc7426bbe37454eaf9a87a7546
Karan-Tatva24 <karan.sarvaiya@etatvasoft.com>
2024-07-23 16:50:58 +0530

    Add tailwind prettier plugin and format all tailwind classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
-      <h1 className="text-3xl text-center">Hi Karan</h1>
+    <main className="bg-grid-white/[0.2] min-h-screen bg-black/[0.96] antialiased">
+      <h1 className="text-center text-3xl">Hi Karan</h1>
     </main>
   );
 }
