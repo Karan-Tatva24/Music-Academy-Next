1bc28a9cf858c971ef698c881107e897ff78b975
Karan-Tatva24 <karan.sarvaiya@etatvasoft.com>
2024-07-23 18:33:48 +0530

    Add featured courses section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="bg-grid-white/[0.2] min-h-screen bg-black/[0.96] antialiased">
       <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
