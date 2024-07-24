693b6ba6b4d1c17432031af30b3898eee76dc3fd
Karan-Tatva24 <karan.sarvaiya@etatvasoft.com>
2024-07-24 09:53:13 +0530

    Add why choose us section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="bg-grid-white/[0.2] min-h-screen bg-black/[0.96] antialiased">
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
     </main>
   );
 }
